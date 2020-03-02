b046272215385183dba4897c42b3ec4f182ff585
HP <mido.essam2016@hotmail.com>
2020-03-02 17:47:08 +0200

    Python

---
+++ whileloop.py
@@ -0,0 +1,29 @@
+#secret_number = 9
+#guess_count = 0
+#guess_limit = 3
+#while guess_count < guess_limit:
+#    guess =int(input("Guess: "))
+#    guess_count +=1
+#    if guess == secret_number:
+#        print("You Win! ")
+#        break
+#else:
+#    print("Sorry")
+
+command = ""
+while True:
+    command=input("> ").lower()
+    if command =="start":
+        print("Car started...")
+    elif command =="stop":
+        print("Car stopped")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry idon't understand you ")
